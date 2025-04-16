1fabc7d9db70b74bd7c8ba99084910ae8bacbe0b
Baumendhra <baumendhra@gmail.com>
2025-04-17 00:18:21 +0530

    initial changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router,Routes,Route,Link } from 'react-router-dom';
+import CropList from './components/CropList';
+import Weather from './components/Weather';
+import Login from './components/Login';
+import Register from './components/Register';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="p-6 bg-green-50 min-h-screen">
+        <nav className="mb-6 flex ap-4">
+          <Link to="/" className="text-green-700 font-bold">Home</Link>
+          <Link to="weather">Weather</Link>
+          <Link to="/login">Login</Link>
+          <Link to="register">Register</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<CropList/>}/>
+          <Route path="weather" element={<Weather/>}/>
+          <Route path="login" element={<Login/>}/>
+          <Route path="register" element={<Register/>}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
